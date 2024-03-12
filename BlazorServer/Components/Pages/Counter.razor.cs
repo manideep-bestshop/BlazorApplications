f2738d845d56dc1afedbdd8cd74563cb067daac2
Admin <Admin@DESKTOP-7T0EOMO>
2024-03-12 23:21:44 +0530

    Add project files.

---
+++ BlazorServer/Components/Pages/Counter.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorServer.Components.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
---
+++ BlazorServer/Components/Pages/CounterBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServer.Components.Pages
+{
+    public class CounterBase:ComponentBase
+    {
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
